8a8c00551dd0b08139e934f0763c74a6b680af58
望月冬夜 <touyamochizukivn@gmail.com>
2022-09-29 17:07:41 +0700

    up

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Smartphone(models.Model):
+    class Meta:
+        verbose_name = 'Smartphone'
+        verbose_name_plural = 'Smmartphones'
+
+    name = models.CharField(_(""), max_length=100),
+    model = models.CharField(_(""), max_length=100),
+    imei = models.CharField(_(""), max_length=100),
+    serial_number = models.CharField(_(""), max_length=100),
+    color = models.CharField(_(""), max_length=100),
+    image = models.CharField(_(""), max_length=100),
+    mfg = models.CharField(_(""), max_length=100),
+    release_date = models.CharField(_(""), max_length=100),
+    specification = models.CharField(_(""), max_length=100),
+    description = models.CharField(_(""), max_length=100),
+    price = models.CharField(_(""), max_length=100),
+
+    def __str__(self):
+        return self.name
+    
